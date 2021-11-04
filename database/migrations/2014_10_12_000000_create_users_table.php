a71f276c712caebaebcf31e01e96ae2021eb3689
yungifez <ifezueyoung@gmail.com>
2021-11-04 23:41:02 +0100

    added school id field to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->foreignId('school_id')->nullable()->constrained();
             $table->timestamps();
         });
     }
