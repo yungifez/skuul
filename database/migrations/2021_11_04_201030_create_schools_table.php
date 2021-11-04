90682a37ecd44da85bb99cbf80946d411bc1a635
yungifez <ifezueyoung@gmail.com>
2021-11-04 21:57:12 +0100

    Created school migration, model and controller
    Created school migration and added some columns
    
    added the fillable property to the school model file
    
    created school controller

---
+++ database/migrations/2021_11_04_201030_create_schools_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('initials')->nullable();
+            $table->string('address')->nullable();
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+}
