47fb2ee67d89752761a024a475b76f92c81256f0
yungifez <ifezueyoung@gmail.com>
2021-11-09 21:28:23 +0100

    created create-school component

---
+++ app/Http/Livewire/CreateSchool.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CreateSchool extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-school');
+    }
+}
---
+++ resources/views/livewire/create-school.blade.php
@@ -0,0 +1,6 @@
+<form action="">
+    <x-adminlte-input name="name" placeholder="Enter name of school" label="School Name"/>
+    <x-adminlte-textarea name="address" placeholder="Enter school branch address" label="School Address"/>
+    <x-adminlte-input name="initials" placeholder="Enter school initials" label="School initials"/>
+    <x-adminlte-button label="Submit" theme="primary" icon="fas fa-paper-plane"/>
+</form>
