eac4df9851136baeb4d4cdda1c2dd0fab1d8841a
yungifez <ifezueyoung@gmail.com>
2021-12-17 21:16:11 +0100

    added getAllSection method

--- app/Services/Section/SectionService.php
+++ app/Services/Section/SectionService.php
@@ -3,12 +3,30 @@
 namespace App\Services\Section;
 
 use App\Models\Section;
+use App\Services\School\SchoolService;
 
 class SectionService
 {
+    //construct method which calls on the SchoolService class
+
+    public function __construct(SchoolService $school)
+    {
+        $this->school = $school;
+    }
+    
     public function getAllSections()
     {
-        return Section::where('');
+        $myClasses = $this->school->getSchoolById(auth()->user()->school_id)->myClasses->all();
+
+        try {
+            $sections = collect();
+            foreach ($myClasses as $myClass) {
+                $sections = $sections->merge($myClass->sections->all());
+            }
+            return $sections;
+        } catch (\Throwable $th) {
+            return session()->flash('danger' ,__('No sections found'));
+        }
     }
 
     public function getSectionById($id)
@@ -25,7 +43,7 @@ class SectionService
         $section = Section::firstOrCreate($records);
 
         if ($section->wasRecentlyCreated) {
-            session()->flash('danger' ,__('Section created successfully'));
+            session()->flash('success' ,__('Section created successfully'));
         }else {
             session()->flash('danger' ,__('Section slready exists'));
         }
