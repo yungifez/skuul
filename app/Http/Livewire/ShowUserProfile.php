6b98b1c62857840fcc9898a6b801919b11cfab33
yungifez <ifezueyoung@gmail.com>
2022-02-07 20:22:48 +0100

    Livewire for user profile

---
+++ app/Http/Livewire/ShowUserProfile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class ShowUserProfile extends Component
+{
+    public User $user;
+    public function render()
+    {
+        return view('livewire.show-user-profile');
+    }
+}
