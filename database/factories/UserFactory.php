df2d52b335750ac93921a32b522144a5eca456e9
Shift <shift@laravelshift.com>
2022-02-09 09:57:14 +0000

    Remove unnecessary `$model` property

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,13 +11,6 @@ use Laravel\Jetstream\Features;
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
