e3c54ef615201e81126c9ff2e466e8ef43cbf611
yungifez <ifezueyoung@gmail.com>
2022-04-08 21:27:48 +0100

    Still trying to fix

--- app/Services/Timetable/TimetableService.php
+++ app/Services/Timetable/TimetableService.php
@@ -54,7 +54,15 @@ class TimetableService
     public function createPdfFromView(string $name, string $view, array $data)
     {
         $pdf = Pdf::loadView($view, $data);
-
+        $pdf->getDomPDF()->setHttpContext(
+            stream_context_create([
+                'ssl' => [
+                    'allow_self_signed'=> TRUE,
+                    'verify_peer' => FALSE,
+                    'verify_peer_name' => FALSE,
+                ]
+            ])
+        );
         return $pdf->download("$name.pdf");
     }
 
