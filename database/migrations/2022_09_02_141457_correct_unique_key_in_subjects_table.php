aad1a1999134a4eac565250226d6842b0f8037f3
Ebuka Ifezue <ifezueyoung@gmail.com>
2022-09-02 15:32:50 +0100

    Fixed wrong unique key

---
+++ database/migrations/2022_09_02_141457_correct_unique_key_in_subjects_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('subjects', static function (Blueprint $table) {
+            $table->dropIndex('subjects_name_school_id_unique');
+            $table->unique(['name', 'my_class_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('subjects', static function (Blueprint $table) {
+            $table->dropIndex('subjects_name_my_class_id_unique');
+            $table->unique(['name', 'school_id']);
+        });
+    }
+};
