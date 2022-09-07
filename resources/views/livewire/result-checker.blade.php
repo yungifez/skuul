19af6524e5163c1253946a82ba9f649e0d51e139
Ebuka Ifezue <ifezueyoung@gmail.com>
2022-09-07 13:11:51 +0100

    Result checker improvements

--- resources/views/livewire/result-checker.blade.php
+++ resources/views/livewire/result-checker.blade.php
@@ -56,44 +56,48 @@
                     </div>
             </form>
         @endif
-        @isset($exams)
-            @foreach ($exams as $exam)
-                <x-adminlte-card title="{{$exam->name}}" theme="info" maximizable>
-                   @if (!$exam->examSlots->isEmpty())
-                        <div class="table-responsive">
-                            <table class="table table-bordered" style="white-space: nowrap">
-                                <tr>
-                                    <th class="text-primary">Subject</th>
-                                    @foreach ($exam->examSlots as $examSlot)
-                                        <th>{{$examSlot->name}} ({{$examSlot->total_marks}})</th>
-                                    @endforeach
-                                    <th class="text-primary">Total ({{$exam->load('examSlots')->examSlots()->pluck('total_marks')->sum()}})</th>
-                                </tr>
-
-                                @foreach ($subjects as $subject)                                
+        @if ($preparedResults == true)
+            @isset($exams)
+                @foreach ($exams as $exam)
+                    <x-adminlte-card title="{{$studentName}}'s result in {{$exam->name}}" theme="info" maximizable>
+                    @if (!$exam->examSlots->isEmpty())
+                            <div class="table-responsive">
+                                <table class="table table-bordered" style="white-space: nowrap">
                                     <tr>
-                                        <th>{{$subject->name}}</th>
+                                        <th class="text-primary">Subject</th>
                                         @foreach ($exam->examSlots as $examSlot)
-                                            <td class="text-center+">
-                                                @if ($examRecords->where('subject_id' , $subject->id)->where('exam_slot_id' , $examSlot->id)->first())
-                                                    {{$examRecords->where('subject_id' , $subject->id)->where('exam_slot_id' , $examSlot->id)->first()->student_marks}}
-                                                @else
-                                                    No record
-                                                @endif
-                                            </td>
+                                            <th>{{$examSlot->name}} ({{$examSlot->total_marks}})</th>
                                         @endforeach
-                                        <th>{{$examRecords->where('subject_id' , $subject->id)->whereIn('exam_slot_id', $exam->examSlots()->pluck('id'))->sum('student_marks')}}</th>
+                                        <th class="text-primary">Total ({{$exam->load('examSlots')->examSlots()->pluck('total_marks')->sum()}})</th>
                                     </tr>
-                                @endforeach
-                            </table>
-                        </div>
 
-                        <p>Total marks obtained: {{$examRecords->whereIn('exam_slot_id', $exam->examSlots()->pluck('id'))->sum('student_marks')}} / {{$exam->load('examSlots')->examSlots()->pluck('total_marks')->sum() * $subjects->count()}}</p>
-                   @else
-                       <p>No exam records found</p>
-                   @endif
-                </x-adminlte-card>      
-            @endforeach
-        @endisset
+                                    @foreach ($subjects as $subject)                                
+                                        <tr>
+                                            <th>{{$subject->name}}</th>
+                                            @foreach ($exam->examSlots as $examSlot)
+                                                <td class="text-center+">
+                                                    @if ($examRecords->where('subject_id' , $subject->id)->where('exam_slot_id' , $examSlot->id)->first())
+                                                        {{$examRecords->where('subject_id' , $subject->id)->where('exam_slot_id' , $examSlot->id)->first()->student_marks}}
+                                                    @else
+                                                        No record
+                                                    @endif
+                                                </td>
+                                            @endforeach
+                                            <th>{{$examRecords->where('subject_id' , $subject->id)->whereIn('exam_slot_id', $exam->examSlots()->pluck('id'))->sum('student_marks')}}</th>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+
+                            <p>Total marks obtained: {{$examRecords->whereIn('exam_slot_id', $exam->examSlots()->pluck('id'))->sum('student_marks')}} / {{$exam->load('examSlots')->examSlots()->pluck('total_marks')->sum() * $subjects->count()}}</p>
+                    @else
+                        <p>No exam records found</p>
+                    @endif
+                    </x-adminlte-card>      
+                @endforeach
+            @endisset
+        @elseif (isset($status))
+            <P>{{$status}}</P>
+        @endif
     </div> 
 </div>
