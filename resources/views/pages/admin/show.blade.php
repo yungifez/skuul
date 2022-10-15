ee814f276a88cd957e4caaa9035a762ef9e78b13
yungifez <ifezueyoung@gmail.com>
2022-10-15 10:41:03 +0100

    Added admin show page

---
+++ resources/views/pages/admin/show.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', __("$admin->name's profile"))
+
+@section('content_header')
+    <h1 class="font-weight-semibold">
+        {{ __("$admin->name's profile") }}
+    </h1>
+
+    @livewire('show-set-school')
+
+    @livewire('breadcrumbs', ['paths' => [
+        ['href'=> route('dashboard'), 'text'=> 'Dashboard'],
+        ['href'=> route('admins.index'), 'text'=> 'admins'],
+        ['href'=> route('admins.show', $admin->id), 'text'=> "View $admin->name's profile", 'active'],
+    ]])
+@endsection
+
+@section('content')
+
+    @livewire('show-admin-profile', ['admin' => $admin])
+
+    @livewire('display-status')
+
+    
+@endsection
