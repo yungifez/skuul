d6e6ff5065aa6dca54e75c1d858272df90764b49
yungifez <ifezueyoung@gmail.com>
2022-12-01 21:20:56 +0000

    Removed whitespace

--- resources/views/emails/account-application/status-changed.blade.php
+++ resources/views/emails/account-application/status-changed.blade.php
@@ -4,27 +4,27 @@
 <x-mail::panel>
 	@switch(strtolower($status))
 		@case('approved')
-			Congratulations, your application has been approved. Log in to see new changes now.
+Congratulations, your application has been approved. Log in to see new changes now.
 			@break
 		@case('application received')
-			Your application has been received, further instructions would be sent to you.
+Your application has been received, further instructions would be sent to you.
 			@break
 		@case('under review')
-			Your application is currently under review, you would be notified when a decision is made.
+Your application is currently under review, you would be notified when a decision is made.
 			@break
 		@case('rejected')
-			Your application was unfortunately rejected.
+Your application was unfortunately rejected.
 			@break
 		@case('user action required')
-			Your action is currently required, if you don't see the reason why contact the school for support.
+Your action is currently required, if you don't see the reason why contact the school for support.
 			@break
 		@default
-		This email has been sent to you because your application status has changed to {{$status}}.
+This email has been sent to you because your application status has changed to {{$status}}.
 	@endswitch
 	<br>
 	<br>
 	@isset($reason)
-		Reason/Message -  {{ $reason }} 
+Reason/Message -  {{ $reason }} 
 	@endisset
 	<x-mail::button url="{{route('dashboard')}}">
 	Log in
@@ -35,7 +35,7 @@
 <br>
 Note that this is an automated email.
 <br>
-	Regards, 
+Regards, 
 <br>
 	{{config('app.name')}}
 
