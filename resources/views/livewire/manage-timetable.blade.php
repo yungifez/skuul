b75b117509c32b7581ef4bbc16aac7763c2d6244
yungifez <ifezueyoung@gmail.com>
2022-12-26 21:44:33 +0000

    Manage timetable

--- resources/views/livewire/manage-timetable.blade.php
+++ resources/views/livewire/manage-timetable.blade.php
@@ -58,7 +58,12 @@
             @endisset
         </div>
 
-        @livewire('loading-spinner')
+        {{-- loading spinner --}}
+        <div class="d-flex justify-content-center">
+            <div wire:loading class="spinner-border" role="status">
+                <p class="sr-only">Loading.....</p>
+            </div>
+        </div>
         @livewire("show-timetable", ['timetable' => $timetable, 'showDescription' => false ], key('show-timetable'))
         
         {{--Create timeslot form--}}
