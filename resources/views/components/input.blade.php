478ce7f2b36a44a96bcb79af0182421541689680
yungifez <ifezueyoung@gmail.com>
2023-01-19 01:20:13 +0000

    Fixed login not working

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,6 +1,6 @@
 <div @class(["$groupClass flex flex-col my-2"])>
     <label for="{{$id}}" @class(["$labelClass font-bold my-3"])>{{$label}}</label>
-    <input id={{$id}} @class(["$class border border-gray-500 p-2 rounded dark:bg-transparent", 'border-red-500' => $errors->has($name)]) {{$attributes}}>
+    <input id={{$id}} name="{{$name}}" @class(["$class border border-gray-500 p-2 rounded dark:bg-transparent", 'border-red-500' => $errors->has($name)]) {{$attributes}}>
     @error($name)
         <p class="text-red-700 my-2">{{$message}}</p>
     @enderror
