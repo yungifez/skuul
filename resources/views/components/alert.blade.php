47ee798b112d7491d78a2e69c639b6d8fd4dde67
yungifez <ifezueyoung@gmail.com>
2023-01-30 19:37:59 +0000

    Added timer and offline indicator to display status

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,13 +1,25 @@
-@props(['colour' => 'bg-red-500', 'title', 'icon' => 'fa fa-ban', 'class' => '', 'id' => 'alert'])
+@props(['colour' => 'bg-red-500', 'title', 'icon' => 'fa fa-ban','stackIcons' => [] , 'class' => '', 'id' => 'alert', 'timeout' => '5000', 'show' => true])
 
-<div @class(["$colour $class p-3 text-white rounded w-full"]) aria-role="alert" x-data="{'showAlert' : true}" x-show="showAlert" x-transition id="{{$id}}" {{$attributes}}>
+<div @class(["$colour $class p-3 text-white rounded w-full"]) aria-role="alert" x-data="{'showAlert' : {{$show}}}" x-show="showAlert" x-transition id="{{$id}}" {{$attributes}} style="display:none">
     <div class="flex gap-3 justify-between">
         <div class="flex gap-3 items-center">
-            <i class="text-xl {{$icon}}"></i>
+            @if (!empty($stackIcons))
+                <span class="fa-stack">
+                    @foreach ($stackIcons as $stackIcon)
+                        <i class="{{$stackIcon}} fa-stack-{{$loop->iteration}}x"></i>
+                    @endforeach
+                </span>
+            @else
+                <i class="text-xl {{$icon}}"></i>
+            @endif
             <p class="text-xl">
                 {{$title}}
             </p>
         </div>
+        @if ($attributes->get('dismissOnTimeout') == true)
+            <span x-init="setTimeout(() => { showAlert = false }, {{$timeout}});"></span>
+        @endif
+        
         <div>
             <i class="fas fa-x text-lg mx-2 cursor-pointer" aria-role="button" aria-hidden="true" @click="showAlert = false">
                 <p class="sr-only">Close Alert</p>
--- resources/views/livewire/display-status.blade.php
+++ resources/views/livewire/display-status.blade.php
@@ -1,20 +1,24 @@
 <div>
-    <div class="fixed flex flex-col items-end top-0 right-0 w-screen lg:w-4/12" id="status-display" x-data="{show: true }" x-inti="setTimeout(() => {show = false}, 10000)" x-show="show">
+    <div class="fixed flex flex-col items-end top-0 right-0 w-screen lg:w-4/12" id="status-display">
         @if (session('danger'))
-            <x-alert colour="bg-red-500" title="Danger" icon="fa fa-do-ban">
+            <x-alert colour="bg-red-500" title="Danger" icon="fa fa-do-ban" dismissOnTimeout="true" >
                 {{ session('danger') }}
             </x-alert>
         @endif
         @if (session('success'))
-            <x-alert colour="bg-green-500" title="Success" icon="fa fa-check">
+            <x-alert colour="bg-green-500" title="Success" icon="fa fa-check" dismissOnTimeout="true" >
                 {{ session('success') }}
             </x-alert>
         @endif
         @if (session('info'))
-            <x-alert colour="bg-yellow-500" title="info">
+            <x-alert colour="bg-yellow-500" title="info" dismissOnTimeout="true">
                 {{ session('info') }}
             </x-alert>
         @endif
-    
+        <x-alert colour="bg-red-500" title="No Internet" :stack-icons="['fa fa-signal', 'fa fa-ban']" show="false">
+            <div  @offline.window="showAlert = true" @online.window="showAlert = false">
+                Your Device Has Gone Offline
+            </div>
+        </x-alert>
     </div>
 </div>
