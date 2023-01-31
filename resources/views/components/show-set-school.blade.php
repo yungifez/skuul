c9121b0ac731a6edf59c76780ee536f762d2e618
yungifez <ifezueyoung@gmail.com>
2023-01-31 00:56:01 +0000

    Made text extra small

--- resources/views/components/show-set-school.blade.php
+++ resources/views/components/show-set-school.blade.php
@@ -1,6 +1,6 @@
 @role('super-admin')
 <div>
-    <p class="text-gray-600 dark:text-gray-200 text-sm md:text-base my-2">
+    <p class="text-gray-600 dark:text-gray-200 text-xs md:text-base my-2">
         @if (auth()->user()->school != null)
             You are currently on {{auth()->user()->school->name}} - {{auth()->user()->school->address}}
         @else
