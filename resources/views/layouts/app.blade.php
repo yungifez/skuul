c37a24430969bf47c633ce98f5a3c870abaf17ef
yungifez <ifezueyoung@gmail.com>
2023-01-31 14:02:00 +0000

    Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
             <div class="lg:flex lg:flex-cols text-gray-900 bg-gray-100 dark:bg-gray-700 dark:text-gray-50 min-h-screen" >
                 <livewire:layouts.menu />
                 <main class="w-full max-w-full overflow-scroll beautify-scrollbar">
-                    <div class="bg-white dark:bg-gray-800 p-4 w-100  ">
+                    <div class="bg-white dark:bg-gray-800 p-4 w-full ">
                         <h1 class="text-3xl my-2 capitalize font-semibold">@yield('page_heading')</h1>
                         <div class="w-full">
                             <x-show-set-school />
