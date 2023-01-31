00f65d5da9f9740daa1072798d2f49b2ff50b266
yungifez <ifezueyoung@gmail.com>
2023-01-31 00:29:58 +0000

    Fixing font size prevent on different devices

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -2,7 +2,7 @@
 
 <ol class="capitalize my-4">
     @isset ($paths)
-        <i class="fa fa-home mr-2 text-sm" aria-hidden="true"></i>
+        <i class="fa fa-home mr-2" aria-hidden="true"></i>
         @foreach ($paths as $path)
             @if (!in_array('active', $path))
                 <li class="breadcrumb-item inline mx-1 text-capitalize text-sm"><a class="text-blue-600" href="{{$path['href']}}">{{__($path['text'])}}</a></li>
