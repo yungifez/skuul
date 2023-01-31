406c908eeb0abee7b8d81b97cdf9841f1f89240a
yungifez <ifezueyoung@gmail.com>
2023-01-31 13:55:54 +0000

    Fixed edit ccount application not working

--- resources/views/livewire/edit-account-application-form.blade.php
+++ resources/views/livewire/edit-account-application-form.blade.php
@@ -5,7 +5,7 @@
     <div class="card-body">
         <form action="{{route('account-applications.update', $applicant->id)}}" method="POST" enctype="multipart/form-data">
             <livewire:edit-user-fields :user="$applicant" />
-            <x-select id="role" name="role_id" label="Change role" ">    
+            <x-select id="role" name="role_id" label="Change role">    
                 @foreach ($roles as $item)
                     <option value="{{$item['id']}}" @selected($applicant->accountApplication && $applicant->accountApplication ? $item['id'] == $applicant->accountApplication->role->id :  null) >{{$item['name']}}</option>
                 @endforeach
