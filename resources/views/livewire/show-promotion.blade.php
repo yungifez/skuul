dbe0a5744940ec290953b83d3664e955a89998fa
yungifez <ifezueyoung@gmail.com>
2023-02-04 01:40:34 +0000

    Got rid of old adminlte files

--- resources/views/livewire/show-promotion.blade.php
+++ resources/views/livewire/show-promotion.blade.php
@@ -1,4 +1,5 @@
-<x-adminlte-card title="Promotion details" theme="primary" icon="">
+<div class="card">
+<div class="card-body">
     <div>
         <h1 class="text-xl lg:text-2xl text-center font-bold my-3">Promotion details</h1>
         <x-show-table :body="[
@@ -9,9 +10,9 @@
         ]"/>
     </div>
     <h4 class="font-bold text-center text-xl lg:text-2xl my-3">Students promoted</h4>
-    <ul class="text-xl">
+    <ul class="">
         @foreach ($students as $student)
             <li>{{$student->name}}</li>
         @endforeach
-    </ul>
-</x-adminlte-card>
+</div>
+</div>
