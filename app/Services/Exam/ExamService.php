a5e6cee75e17f0442ce3db01ab6789ec875c3da3
yungifez <ifezueyoung@gmail.com>
2023-03-20 13:58:04 +0000

    Prevent publishing of incomplete exam

--- app/Services/Exam/ExamService.php
+++ app/Services/Exam/ExamService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\Exam;
 
+use App\Exceptions\EmptyRecordsException;
+use App\Exceptions\InvalidValueException;
 use App\Models\Exam;
 use App\Models\Semester;
 use App\Models\Subject;
@@ -110,11 +112,15 @@ class ExamService
     /**
      * Set result publish status for exam.
      *
-     *
+     *@throws 
      * @return void
      */
     public function setPublishResultStatus(Exam $exam, bool $status)
     {
+        if ($exam->examSlots()->count() <= 0 && $status == 1) {
+            throw new EmptyRecordsException("Cannot publish result for exam without exam slots", 1);
+        }
+        
         $exam->publish_result = $status;
         $exam->save();
     }
