e8275f7c9333a62fa298fd54a9a0b69b028602cb
Marvellous <ifezueyoung@gmail.com>
2023-08-03 16:55:42 +0100

    remove menu ghost on load

--- resources/views/livewire/layouts/menu.blade.php
+++ resources/views/livewire/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<nav class="fixed lg:sticky inset-0 h-screen shadow-lg flex duration-250 text-gray-100 transition-all dark:lg:border-r dark:border-white z-40" :class="{' w-0 overflow-hidden lg:flex lg:w-20' : menuOpen == false, 'w-full md:w-3/6 lg:w-72' : menuOpen == true}">
+<nav class="fixed w-0 lg:sticky inset-0 h-screen shadow-lg flex duration-250 text-gray-100 transition-all dark:lg:border-r dark:border-white z-40" :class="{' w-0 overflow-hidden lg:flex lg:w-20' : menuOpen == false, 'w-full md:w-3/6 lg:w-72' : menuOpen == true}">
     <aside class=" bg-gray-800 dark:bg-gray-900 shadow-md shadow-black border-x border-black overflow-scroll beautify-scrollbar">
         <a href="{{route('home')}}" class="flex md:hidden items-center justify-center border-b border-gray-200 p-4">
             <img src="{{asset(auth()->user()->school->logoURL ?? config('app.logo'))  }}" alt="" class="rounded-full w-14 h-14 border border-gray-200 shadow-md">
